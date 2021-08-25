0e34b4a684defa3d397fc685320e10ca6d3347e8
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-24 18:00:00 -0700

    creating tracks

--- track-server/src/routes/trackRoutes.js
+++ track-server/src/routes/trackRoutes.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 const mongoose = require('mongoose')
 const requireAuth = require('../middlewares/requireAuth')
@@ -11,6 +12,22 @@ router.use(requireAuth)
 router.get('/tracks', async (req, res) => {
   const tracks = await Track.find({ userId: req.user._id })
   res.send(tracks)
+})
+
+router.post('/tracks', async (req, res) => {
+  const { name, locations } = req.body
+
+  if (!name || !locations)
+    return res
+      .status(422)
+      .send({ error: 'You must provide a name and locations' })
+  try {
+    const track = new Track({ name, locations, userId: req.user._id })
+    await track.save()
+    res.send(track)
+  } catch (error) {
+    return res.status(422).send({ error: err.message })
+  }
 })
 
 module.exports = router
