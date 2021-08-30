566563dc451c6af5d957a40f53e001a08593f483
cohlerCoded <jeffcohler@cohlercoded.com>
2021-08-30 14:30:16 -0700

    Sign in screen

--- trackFrontend/src/screens/SigninScreen.jsx
+++ trackFrontend/src/screens/SigninScreen.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { Text, Input, Button } from 'react-native-elements'
 
 const SigninScreen = ({ navigation }) => {
   return (
     <View>
-      <Text style={{ fontSize: 48 }}>SigninScreen</Text>
-      <Button title='Signup' onPress={() => navigation.navigate('Signup')} />
+      <Text h3 h3Style={{ marginVertical: 15, textAlign: 'center' }}>
+        Sign In
+      </Text>
+      <Input label='Email' />
+      <Input label='Password' />
       <Button
-        title='TrackList'
+        title='Sign In'
+        loading={false}
+        loadingProps={{ size: 'small', color: 'white' }}
+        buttonStyle={{
+          backgroundColor: 'rgba(78, 116, 289, 1)',
+          borderRadius: 100,
+        }}
+        titleStyle={{ fontWeight: 'bold', fontSize: 23 }}
+        containerStyle={{
+          height: 50,
+          width: 200,
+          marginVertical: 10,
+          alignSelf: 'center',
+        }}
         onPress={() => navigation.navigate('TrackList')}
       />
     </View>
