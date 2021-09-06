c5e0ce873d72ec4f9bc7d9254847d63b1f0ab25f
cohlerCoded <jeffcohler@cohlercoded.com>
2021-09-05 21:50:03 -0700

    SafeAreaView

--- trackFrontend/src/api/tracker.js
+++ trackFrontend/src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://04e5-47-232-164-62.ngrok.io',
+  baseURL: 'http://62d5-47-232-164-62.ngrok.io',
 })
--- trackFrontend/src/screens/AccountScreen.jsx
+++ trackFrontend/src/screens/AccountScreen.jsx
@@ -1,32 +1,35 @@
 import React, { useState, useContext } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Text, Input, Button } from 'react-native-elements'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { Context as AuthContext } from '../context/authContext'
 
 const AccountScreen = () => {
   const { signout } = useContext(AuthContext)
   return (
     <View>
-      <Text h3 h3Style={{ marginVertical: 30, textAlign: 'center' }}>
-        Account Settings
-      </Text>
-      <Button
-        title='Sign Out'
-        loading={false}
-        loadingProps={{ size: 'small', color: 'white' }}
-        buttonStyle={{
-          backgroundColor: 'red',
-          borderRadius: 100,
-        }}
-        titleStyle={{ fontWeight: 'bold', fontSize: 23 }}
-        containerStyle={{
-          height: 50,
-          width: 200,
-          marginVertical: 10,
-          alignSelf: 'center',
-        }}
-        onPress={signout}
-      />
+      <SafeAreaView forceInset={{ top: 'always' }}>
+        <Text h3 h3Style={{ marginVertical: 30, textAlign: 'center' }}>
+          Account Settings
+        </Text>
+        <Button
+          title='Sign Out'
+          loading={false}
+          loadingProps={{ size: 'small', color: 'white' }}
+          buttonStyle={{
+            backgroundColor: 'red',
+            borderRadius: 100,
+          }}
+          titleStyle={{ fontWeight: 'bold', fontSize: 23 }}
+          containerStyle={{
+            height: 50,
+            width: 200,
+            marginVertical: 10,
+            alignSelf: 'center',
+          }}
+          onPress={signout}
+        />
+      </SafeAreaView>
     </View>
   )
 }
