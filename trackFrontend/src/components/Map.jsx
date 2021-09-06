2dd11c37d3d8ee10d792b13e24c21367a67413a0
cohlerCoded <jeffcohler@cohlercoded.com>
2021-09-05 22:18:28 -0700

    create a track boiler plate

---
+++ trackFrontend/src/components/Map.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Map = () => {
+  return (
+    <View>
+      <Text>Map</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default Map
--- trackFrontend/src/screens/TrackCreateScreen.jsx
+++ trackFrontend/src/screens/TrackCreateScreen.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-elements'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Map from '../components/Map'
 
 const TrackCreateScreen = () => {
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text h2 h2Style={{ marginVertical: 30, textAlign: 'center' }}>
+        Creat a New Track
+      </Text>
+      <Map />
+    </SafeAreaView>
   )
 }
 
