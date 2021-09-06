9480271437a8d26c8c928cc372c085f3647e41d6
cohlerCoded <jeffcohler@cohlercoded.com>
2021-09-06 13:47:41 -0700

    request location permission

--- trackFrontend/src/screens/TrackCreateScreen.jsx
+++ trackFrontend/src/screens/TrackCreateScreen.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import { requestForegroundPermissionsAsync } from 'expo-location'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Map from '../components/Map'
-
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null)
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync()
+      if (!granted) {
+        throw new Error('Location permission not granted')
+      }
+    } catch (error) {
+      setErr(error)
+    }
+  }
+
+  useEffect(() => {
+    startWatching()
+  }, [])
+
   return (
     <SafeAreaView>
       <Text h2 h2Style={{ marginVertical: 30, textAlign: 'center' }}>
-        Creat a New Track
+        Create a New Track
       </Text>
       <Map />
+      {err && <Text>Please Enable Location Services</Text>}
     </SafeAreaView>
   )
 }
