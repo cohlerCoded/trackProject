64c12c20d4638e5ebee6d0078ebcac7b5e8e633b
cohlerCoded <jeffcohler@cohlercoded.com>
2021-09-08 17:38:54 -0700

    extracting logic to a hook

---
+++ trackFrontend/src/hooks/useLocation.js
@@ -0,0 +1,35 @@
+import {
+  Accuracy,
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+} from 'expo-location'
+import { useEffect, useState } from 'react'
+
+export default (callback) => {
+  const [err, setErr] = useState(null)
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync()
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        callback
+      )
+      if (!granted) {
+        throw new Error('Location permission not granted')
+      }
+    } catch (error) {
+      setErr(error)
+    }
+  }
+
+  useEffect(() => {
+    startWatching()
+  }, [])
+
+  return [err]
+}
--- trackFrontend/src/screens/TrackCreateScreen.jsx
+++ trackFrontend/src/screens/TrackCreateScreen.jsx
@@ -1,43 +1,14 @@
 import '../_mockLocation'
-import {
-  requestForegroundPermissionsAsync,
-  watchPositionAsync,
-  Accuracy,
-} from 'expo-location'
-import React, { useEffect, useState, useContext } from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useContext } from 'react'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Map from '../components/Map'
 import { Context as LocationContext } from '../context/locationContext'
+import useLocation from '../hooks/useLocation'
 
 const TrackCreateScreen = () => {
   const { addLocation } = useContext(LocationContext)
-  const [err, setErr] = useState(null)
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync()
-      await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        (location) => addLocation(location)
-      )
-      if (!granted) {
-        throw new Error('Location permission not granted')
-      }
-    } catch (error) {
-      setErr(error)
-    }
-  }
-
-  useEffect(() => {
-    startWatching()
-  }, [])
-
+  const [err] = useLocation((location) => addLocation(location))
   return (
     <SafeAreaView>
       <Text h2 h2Style={{ marginVertical: 30, textAlign: 'center' }}>
