de0725f1acd4a3076679baa6f8bfd7e12b68c691
cohlerCoded <jeffcohler@cohlercoded.com>
2021-09-09 21:30:45 -0700

    updates to locationContext

--- trackFrontend/src/context/locationContext.js
+++ trackFrontend/src/context/locationContext.js
@@ -3,23 +3,38 @@ import createDataContext from './createDataContext'
 const locationReducer = (state, action) => {
   switch (action.type) {
     case 'START_RECORDING':
+      return { ...state, recording: true }
     case 'STOP_RECORDING':
-    case 'ADD_LOCATION':
+      return { ...state, recording: false }
+    case 'ADD_CURRENT_LOCATION':
       return { ...state, currentLocation: action.payload }
+    case 'CHANGE_NAME':
+      return { ...state, name: action.payload }
+    case 'ADD_LOCATION':
+      return { ...state, locations: [...state.locations, action.payload] }
     default:
       return state
   }
 }
 
-const startRecording = (dispatch) => () => {}
-const stopRecording = (dispatch) => () => {}
-const addLocation = (dispatch) => (location) => {
-  console.log('tracking')
-  dispatch({ type: 'ADD_LOCATION', payload: location })
+const changeName = (dispatch) => (name) => {
+  dispatch({ type: 'CHANGE_NAME', payload: name })
+}
+const startRecording = (dispatch) => () => {
+  dispatch({ type: 'START_RECORDING' })
+}
+const stopRecording = (dispatch) => () => {
+  dispatch({ type: 'STOP_RECORDING' })
+}
+const addLocation = (dispatch) => (location, recording) => {
+  dispatch({ type: 'ADD_CURRENT_LOCATION', payload: location })
+  if (recording) {
+    dispatch({ type: 'ADD_LOCATION', payload: location })
+  }
 }
 
 export const { Provider, Context } = createDataContext(
   locationReducer,
-  { startRecording, stopRecording, addLocation },
-  { recording: false, locations: [], currentLocation: null }
+  { startRecording, stopRecording, addLocation, changeName },
+  { name: '', recording: false, locations: [], currentLocation: null }
 )
--- trackFrontend/src/screens/TrackCreateScreen.jsx
+++ trackFrontend/src/screens/TrackCreateScreen.jsx
@@ -9,8 +9,10 @@ import { withNavigation, withNavigationFocus } from 'react-navigation'
 import TrackForm from '../components/TrackForm'
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { addLocation } = useContext(LocationContext)
-  const [err] = useLocation(isFocused, addLocation)
+  const { state, addLocation } = useContext(LocationContext)
+  const [err] = useLocation(isFocused, (location) => {
+    addLocation(location, satte.recording)
+  })
 
   return (
     <SafeAreaView>
