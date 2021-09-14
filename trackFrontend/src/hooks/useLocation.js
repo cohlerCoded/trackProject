c3fe70032d643c3f71b6160b4d329591604a7453
cohlerCoded <jeffcohler@cohlercoded.com>
2021-09-14 12:45:32 -0700

    subcriber not a function

--- trackFrontend/src/hooks/useLocation.js
+++ trackFrontend/src/hooks/useLocation.js
@@ -35,7 +35,7 @@ export default (shouldTrack, callback) => {
       if (subscriber) {
         subscriber.remove()
       }
-      subscriber(null)
+      subscriber = null
     }
     return () => {
       if (subscriber) {
