867be6ea917fc6c213069d4ef17033b00db2fa91
cohlerCoded <jeffcohler@cohlercoded.com>
2021-09-13 23:12:01 -0700

    more on avoiding stale references

--- trackFrontend/src/hooks/useLocation.js
+++ trackFrontend/src/hooks/useLocation.js
@@ -7,39 +7,40 @@ import { useEffect, useState } from 'react'
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null)
-  const [subscriber, setSubscriber] = useState(null)
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync()
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        callback
-      )
-      setSubscriber(sub)
-      if (!granted) {
-        throw new Error('Location permission not granted')
-      }
-    } catch (error) {
-      setErr(error)
-    }
-  }
 
   useEffect(() => {
+    let subscriber
+    const startWatching = async () => {
+      try {
+        const { granted } = await requestForegroundPermissionsAsync()
+        const subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10,
+          },
+          callback
+        )
+        if (!granted) {
+          throw new Error('Location permission not granted')
+        }
+      } catch (error) {
+        setErr(error)
+      }
+    }
+
     if (shouldTrack) {
       startWatching()
     } else {
-      subscriber.remove()
-      setSubscriber(null)
+      if (subscriber) {
+        subscriber.remove()
+      }
+      subscriber(null)
     }
     return () => {
       subscriber && subscriber.remove()
     }
-  }, [shouldTrack, callback])
+  }, [shouldTrack, callback, subscriber])
 
   return [err]
 }
