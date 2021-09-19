8d267adafa94d1b49d17170d74452ddfe585169b
cohlerCoded <jeffcohler@cohlercoded.com>
2021-09-19 09:56:38 -0700

    showing track details

--- trackFrontend/src/screens/TrackDetailsScreen.jsx
+++ trackFrontend/src/screens/TrackDetailsScreen.jsx
@@ -1,23 +1,39 @@
 import React, { useContext } from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
+import MapView, { Polyline, Marker } from 'react-native-maps'
 import { Context as TrackContext } from '../context/trackContext'
 
 const TrackDetailsScreen = ({ navigation }) => {
   const { state } = useContext(TrackContext)
   const _id = navigation.getParam('_id')
+
   const track = state.find((t) => t._id === _id)
-  console.log(state)
-  console.log('track = ', track)
+  const initalCoords = track.locations[0].coords
 
   return (
     <View>
       <Text style={{ fontSize: 48 }}>{track.name}</Text>
-      <Button
-        title='TrackList'
-        onPress={() => navigation.navigate('TrackList')}
-      />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          ...initalCoords,
+          longitudeDelta: 0.01,
+          latitudeDelta: 0.01,
+        }}
+      >
+        <Polyline
+          lineDashPattern={[1]}
+          coordinates={track.locations.map((location) => location.coords)}
+        />
+      </MapView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  },
+})
 
 export default TrackDetailsScreen
