c9b9e4ff8f14f89569d26a03a390bb92b79fcaa8
cohlerCoded <jeffcohler@cohlercoded.com>
2021-09-22 09:57:40 -0700

    ConfirmBox component

---
+++ trackFrontend/src/components/ConfirmBox.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { View, Button, StyleSheet, Alert } from 'react-native'
+
+export default function ConfirmBox() {
+  const [showBox, setShowBox] = useState(true)
+
+  const showConfirmDialog = () => {
+    return Alert.alert(
+      'Are your sure?',
+      'Are you sure you want to remove this beautiful box?',
+      [
+        // The "Yes" button
+        {
+          text: 'Yes',
+          onPress: () => {
+            setShowBox(false)
+          },
+        },
+        // The "No" button
+        // Does nothing but dismiss the dialog when tapped
+        {
+          text: 'No',
+        },
+      ]
+    )
+  }
+
+  return (
+    <View style={styles.screen}>
+      {showBox && <View style={styles.box}></View>}
+      <Button title='Delete' onPress={() => showConfirmDialog()} />
+    </View>
+  )
+}
+
+// Kindacode.com
+// Just some styles
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 300,
+    height: 300,
+    backgroundColor: 'red',
+    marginBottom: 30,
+  },
+  text: {
+    fontSize: 30,
+  },
+})
