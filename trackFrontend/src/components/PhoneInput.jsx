55e0813e8a6219aa4414945230d923c87b9bafab
cohlerCoded <jeffcohler@cohlercoded.com>
2021-10-04 14:19:19 -0700

    input changes

--- trackFrontend/src/components/PhoneInput.jsx
+++ trackFrontend/src/components/PhoneInput.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { View, TextInput } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 
 const PhoneInput = () => {
   return (
-    <View>
-      <TextInput maxLength={3} />
-      <TextInput maxLength={3} />
-      <TextInput maxLength={4} />
+    <View style={{ marginLeft: 10 }}>
+      <Text style={{ fontWeight: 'bold', fontSize: 16, color: 'grey' }}>
+        Phone Number
+      </Text>
+      <View style={{ display: 'flex', flexDirection: 'row' }}>
+        <TextInput
+          maxLength={3}
+          style={{ fontSize: 18, borderBottomWidth: 1, height: 48 }}
+        />
+        <TextInput maxLength={3} />
+        <TextInput maxLength={4} />
+      </View>
     </View>
   )
 }
